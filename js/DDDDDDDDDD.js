0c49b94753fc00aed41b40edacec193d62372161
John <johnheslop06@gmail.com>
2024-06-16 15:24:35 +0100

    Secret in home page navbar

---
+++ js/DDDDDDDDDD.js
@@ -0,0 +1,16 @@
+var AboutBefore = document.getElementById("AboutBefore")
+var AboutAfter = document.getElementById("AboutAfter")
+var SpecialText = document.getElementById("SpecialText")
+
+var RNG = Math.floor(Math.random() * 343)
+
+if (RNG == 0) {
+    var TextRNG = Math.floor(Math.random() * 3)
+    if (TextRNG == 0) { AboutBefore.innerHTML = "FINDME"}
+    if (TextRNG == 1) { AboutBefore.innerHTML = "ISEEYOU"}
+    if (TextRNG == 2) { AboutBefore.innerHTML = "HEARME"}
+    AboutAfter.innerHTML = "343vsc"
+    SpecialText.setAttribute("href", "../343vsc")
+    AboutBefore.style.textShadow = "0 0 10px red"
+    AboutAfter.style.textShadow = "0 0 10px red"
+}
